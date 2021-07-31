dc4abfd8a5f165b2d7f05ed761dd703e102b9f34
Xingzhi <xingzhi2107@foxmail.com>
2021-07-31 21:04:38 +0800

    react-ts，配置eslint

---
+++ react-ts/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'react',
+    'react-app',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    browser: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'no-unused-vars': ['error', {args: 'none'}],
+
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+
+    'react/no-multi-comp': 'error',
+    'react/button-has-type': 'error',
+    'react/destructuring-assignment': ['error', 'always', {ignoreClassFields: true}],
+    'react/jsx-closing-tag-location': 'error',
+    'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'always' }],
+    'react/jsx-max-depth': ['error', { 'max': 3 }],
+    'react/jsx-max-props-per-line': ['error', { maximum: 3, when: 'always'}]
+  },
+};
--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -3,21 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const test = 'asdf';
   return (
     <div className="App">
-      <header className="App-header">
+      <header
+        className="App-header"
+        id="asdfasdfadfasdf"
+        title="asdf asdkfajsdkf asdf"
+      >
+        <div>
+          <div>
+            <div>
+              <div>
+                <div>
+                  <div />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {'Edit '}
+          <code>{'src/App.tsx'}</code> {'and save to reload.'}
         </p>
+        <img src="https://glowing.com/1.jpg" alt={test} />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {'Learn React'}
         </a>
+        <button type="reset">{'test'}</button>
       </header>
     </div>
   );
